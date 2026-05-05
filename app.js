c29dab51907d463357557844346e4389d0ba492f
jhonatanfavero <jhonyroxbad@gmail.com>
2026-05-04 22:45:04 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1187,7 +1187,7 @@ window.addEventListener('beforeinstallprompt', (e) => {
 // --- REGISTRO DO SERVICE WORKER (PWA) ---
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('./sw.js')
       .then(registration => {
         console.log('Service Worker registrado com sucesso:', registration.scope);
       })

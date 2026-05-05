26b46a91f36cb61511df58d003f8fe4f206a5a44
jhonatanfavero <jhonyroxbad@gmail.com>
2026-05-04 22:27:58 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'timeblock-v2';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js'
+  // CDNs e fontes externas ficam de fora para evitar falhas de CORS na instalação do SW
+];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});

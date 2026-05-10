67e772ead439e8a02c22109007fde516515f02b9
jhonatanfavero <jhonyroxbad@gmail.com>
2026-05-10 15:32:07 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+// V3.0 - v40.1 (Super Gabinete: 3 abas no list-sheet + Notas livres)
+const CACHE_NAME = 'timeblock-v40-1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js'
+];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});

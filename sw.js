ef9fdc58dd70f8058b1397bf8983c18c839771cd
jhonatanfavero <jhonyroxbad@gmail.com>
2026-05-10 14:33:31 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+// V3.0 - v37 (Padding dinâmico do header via offsetHeight)
+const CACHE_NAME = 'timeblock-v37';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js'
+];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
